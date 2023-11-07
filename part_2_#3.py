2f74c0255d5947daf00a5a6530aed4a9ecd48206
Katerina <kulibinka1987@gmail.com>
2023-11-07 19:28:05 +0300

    add part_2_#3

---
+++ part_2_#3.py
@@ -0,0 +1,34 @@
+# Задача 3
+# Построить:
+# 1) график функции, заданной на интервале [a,b]:
+
+# Подключим модуль для построения графиков функций, дадим ему имя plt
+import matplotlib.pyplot as plt
+
+# Опишем функцию:
+def f_x(x):
+   y = x ** 3 - 6 * x ** 2 +  x + 5
+   return y
+
+# Зададим начало a и конец  b интервала построения функции, задав количество точек построения. Вычислим шаг:
+a = -2
+b = 6
+n =100
+h = (b-a)/(n-1)
+
+# Сформируем списки со значениями аргументов и значениями функции в них:
+x_list = [a + h * i for i in range(n)]
+f_list = [f_x(x) for x in x_list]
+
+# Построим линию графика, установим для нее цвет и толщину:
+line = plt.plot(x_list, f_list)
+plt.setp(line, color="blue", linewidth=2)
+
+# Выведем 2 оси, установим для них позицию zero:
+plt.gca().spines["left"].set_position("zero")
+plt.gca().spines["bottom"].set_position("zero")
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+
+# Отобразим область построения:
+plt.show()
